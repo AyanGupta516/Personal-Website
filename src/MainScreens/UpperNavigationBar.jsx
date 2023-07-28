8ed07d8f6f88d90d149805e87b9e70c6e7edae8b
AyanGupta516 <109633512+AyanGupta516@users.noreply.github.com>
2023-07-28 12:32:55 -0500

    Changes

--- src/MainScreens/UpperNavigationBar.jsx
+++ src/MainScreens/UpperNavigationBar.jsx
@@ -11,11 +11,11 @@ export default function UpperNavigationBar() {
     }
     return (
             <div style={{position: 'fixed', top: '0', left: '0', display: 'flex', justifyContent: 'flex-end', width: '100vw', alignItems:'center', height: '7.5%', backgroundColor: '#252738', overflow: 'auto', zIndex: 1 }}>
-                <NavLink to = '/'  onMouseEnter = {() => handleHovered('Home')} onMouseLeave = {removehover} style = {{color: isHovered === 'Home' ? '#3BBA9C': 'white' , marginRight: '4rem', fontWeight:'bold', fontSize: '1.15rem'}}> Home </NavLink>
-                <NavLink to = '/AboutMe' onMouseEnter = {() => handleHovered('AboutMe')} onMouseLeave = {removehover} style = {{color: isHovered === 'AboutMe' ? '#3BBA9C': 'white', marginRight: '4rem', fontWeight:'bold', fontSize: '1.15rem'}}> About</NavLink>
-                <NavLink to = '/Experience' onMouseEnter = {() => handleHovered('Experience')} onMouseLeave = {removehover} style = {{color: isHovered === 'Experience' ? '#3BBA9C': 'white',  marginRight: '4rem', fontWeight:'bold', fontSize: '1.15rem'}}> Projects & Experience </NavLink>
-                <NavLink to = '/Skills' onMouseEnter = {() => handleHovered('Skills')} onMouseLeave = {removehover} style = {{color: isHovered === 'Skills' ? '#3BBA9C': 'white', marginRight: '4rem', fontWeight:'bold', fontSize: '1.15rem'}}> Skills </NavLink>
-                <NavLink to = '/Contact' onMouseEnter = {() => handleHovered('Contact')} onMouseLeave = {removehover} style = {{color: isHovered === 'Contact' ? '#3BBA9C': 'white', marginRight: '4rem', fontWeight:'bold', fontSize: '1.15rem'}}> Contact</NavLink>
+                <NavLink to = '/'  onMouseEnter = {() => handleHovered('Home')} onMouseLeave = {removehover} style = {{color: isHovered === 'Home' ? '#3BBA9C': 'white' , marginRight: '4%', fontWeight:'bold', fontSize: '1.35vw'}}> Home </NavLink>
+                <NavLink to = '/AboutMe' onMouseEnter = {() => handleHovered('AboutMe')} onMouseLeave = {removehover} style = {{color: isHovered === 'AboutMe' ? '#3BBA9C': 'white', marginRight: '4%', fontWeight:'bold', fontSize: '1.35vw'}}> About</NavLink>
+                <NavLink to = '/Experience' onMouseEnter = {() => handleHovered('Experience')} onMouseLeave = {removehover} style = {{color: isHovered === 'Experience' ? '#3BBA9C': 'white',  marginRight: '4%', fontWeight:'bold', fontSize: '1.35vw'}}> Projects & Experience </NavLink>
+                <NavLink to = '/Skills' onMouseEnter = {() => handleHovered('Skills')} onMouseLeave = {removehover} style = {{color: isHovered === 'Skills' ? '#3BBA9C': 'white', marginRight: '4%', fontWeight:'bold', fontSize: '1.35vw'}}> Skills </NavLink>
+                <NavLink to = '/Contact' onMouseEnter = {() => handleHovered('Contact')} onMouseLeave = {removehover} style = {{color: isHovered === 'Contact' ? '#3BBA9C': 'white', marginRight: '4%', fontWeight:'bold', fontSize: '1.35vw'}}> Contact</NavLink>
             </div>
     )
 }
