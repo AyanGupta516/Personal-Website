6e47ccd6283a255f674b92977473de2cb9e27d97
AyanGupta516 <109633512+AyanGupta516@users.noreply.github.com>
2023-07-29 11:46:41 -0500

    Changes

--- src/ExperienceScreens/SE.jsx
+++ src/ExperienceScreens/SE.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 export default function SE() {
     return (
-        <div>
-            <h1 style = {{color: '#3BBA9C', position: 'absolute', top: '7.5%', left:'50%', transform: 'translateX(-50%)', width: '1250px'}}> System Emulator </h1>
-            <ul style = {{textAlign: 'left', listStyle: 'disc', marginLeft: '-25px', width: '1250px', marginTop: '-100px'}}>
-                    <li style = {{color: 'lightgrey', fontSize: '20px', marginBottom: '40px'}}> Using C, I implemented a basic System Emulator that processed Assembly instructions and replicated a cache system</li>
-                    <li style = {{color: 'lightgrey', fontSize: '20px', marginBottom: '40px'}}> I implemented Instruction Pipelining, including fetching, decoding, and excuting assembly instructions that were given. In addition, I handled any data or control hazards that arised while processing </li>
-                    <li style = {{color: 'lightgrey', fontSize: '20px', marginBottom: '40px'}}> In addition I implemented a basic cache system, that used the LRU policy to evict and bring in new cache lines into the cache </li>
+        <div style = {{display: 'flex', flexDirection: 'column', aligItems: 'center', 'alignItems': 'center'}}>
+            <h1 style = {{color: '#3BBA9C', width: '30vw', fontSize: '3rem', marginTop: '-20%'}}> System Emulator </h1>
+            <ul style = {{textAlign: 'left', listStyle: 'disc', marginLeft: '-2.5%', width: '90vw'}}>
+                    <li style = {{color: 'lightgrey', fontSize: '1.25rem', marginBottom: '3.5%'}}> Using C, I implemented a basic System Emulator that processed Assembly instructions and replicated a cache system</li>
+                    <li style = {{color: 'lightgrey', fontSize: '1.25rem', marginBottom: '3.5%'}}> I implemented Instruction Pipelining, including fetching, decoding, and excuting assembly instructions that were given. In addition, I handled any data or control hazards that arised while processing </li>
+                    <li style = {{color: 'lightgrey', fontSize: '1.25rem', marginBottom: '3.5%'}}> In addition I implemented a basic cache system, that used the LRU policy to evict and bring in new cache lines into the cache </li>
                 </ul>
         </div>
     )
