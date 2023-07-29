e23bf03fcb3b1c42c4484d05291848bdc4b40266
AyanGupta516 <109633512+AyanGupta516@users.noreply.github.com>
2023-07-28 22:25:48 -0500

    Changes

--- src/ExperienceScreens/UHResearch.jsx
+++ src/ExperienceScreens/UHResearch.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import myVideo from '../assets/SUMO.mp4'
 export default function UHResearch() {
     return (
-        <>
-            <h1 style = {{color: '#3BBA9C', position: 'absolute', top: '7.5%', left:'50%', transform: 'translateX(-50%)', width: '800px'}}> Real-Time Systems </h1>
-            <p  style = {{color: 'white', position: 'absolute', top: '20.5%', left:'50%', transform: 'translateX(-50%)', fontSize: '18px'}}> Using SUMO, a traffic simulator interface, to run traffic simulations on custom traffic networks through Python scripts configuring the settings of the simulation, such as start and end location, number of vehicles, etc. Working on developing a better way to calculate costs for edges to route vehicles based on Djikstra's Algorithim and possibly implement a Deep Reinforcement Learning model to also route vehicles</p>
-            <div style = {{height: '400px', width: '600px', marginTop: '225px'}}>
-                <video muted controls style={{ width: '100%', height: '100%'}} >
+        <div style = {{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+
+            <h1 style = {{color: '#3BBA9C'}}> Real Time Traffic Routing </h1>
+            <p  style = {{color: 'white',fontSize: '110%', width: '50vw', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '-1%'}}> Using SUMO, a traffic simulator interface, to run traffic simulations on custom traffic networks through Python scripts configuring the settings of the simulation, such as start and end location, number of vehicles, etc. Working on developing a better way to calculate costs for edges to route vehicles based on Djikstra's Algorithim and possibly implement a Deep Reinforcement Learning model to also route vehicles</p>
+            <div style = {{height: '45vh', width: '45vw', display: 'flex', alignItems: 'center', margin: 'auto'}}>
+                <video muted controls style={{ width: '100%', height: '100%', marginTop: '5%'}} >
                     <source src={myVideo} type="video/mp4" />
                 </video>
-                <p style = {{fontSize: '22px', color: '#BB8FCE', position: 'absolute', left: '50%', transform: 'translateX(-50%)', top: '86%'}}> Sample Simulation </p>
             </div>
             
-        </>
+        </div>
     )
 }
