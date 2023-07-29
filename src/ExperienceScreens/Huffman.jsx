167bed134afafaaed3a6cbf09ef9f0a9d6b169ce
AyanGupta516 <109633512+AyanGupta516@users.noreply.github.com>
2023-07-29 18:04:57 -0500

    Changes

--- src/ExperienceScreens/Huffman.jsx
+++ src/ExperienceScreens/Huffman.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Image from '../assets/Huffman.png'
 export default function Huffman() {
     return (
-        <div>
-            <h1 style = {{color: '#3BBA9C', position: 'absolute', top: '7.5%', left:'50%', transform: 'translateX(-50%)', width: '1250px'}}> Huffman Compression & Decompression Algorithim </h1>
+        <div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center'}}>
+            <h1 style = {{color: '#3BBA9C', width: '75vw', marginTop: '-10%'}}> Huffman Compression & Decompression Algorithim </h1>
             <ul style = {{textAlign: 'left', listStyle: 'disc', marginLeft: '-25px', width: '1250px', marginTop: '-225px'}}>
                     <li style = {{color: 'lightgrey', fontSize: '18px', marginBottom: '20px'}}> Using Java, I implemented Huffman's Compression & Decompression Algorithim, which encondes more common characters with less bits</li>
                     <li style = {{color: 'lightgrey', fontSize: '18px', marginBottom: '20px'}}> I constructed a Huffman tree, by reading 8 bits at a time, and storing the frequencies of characters to create new encondings. </li>
--- src/ExperienceScreens/SE.jsx
+++ src/ExperienceScreens/SE.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React from 'react';
+
 export default function SE() {
-    return (
-        <div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start'}}>
-            <h1 style = {{color: '#3BBA9C', width: '30vw', fontSize: '3rem', marginTop: '-15%'}}> System Emulator </h1>
-            <ul style = {{textAlign: 'left', listStyle: 'disc', marginLeft: '-2.5%', width: '75vw'}}>
-                    <li style = {{color: 'lightgrey', fontSize: '1.25rem', marginBottom: '3.5%'}}> Using C, I implemented a basic System Emulator that processed Assembly instructions and replicated a cache system</li>
-                    <li style = {{color: 'lightgrey', fontSize: '1.25rem', marginBottom: '3.5%'}}> I implemented Instruction Pipelining, including fetching, decoding, and excuting assembly instructions that were given. In addition, I handled any data or control hazards that arised while processing </li>
-                    <li style = {{color: 'lightgrey', fontSize: '1.25rem', marginBottom: '3.5%'}}> In addition I implemented a basic cache system, that used the LRU policy to evict and bring in new cache lines into the cache </li>
-                </ul>
-        </div>
-    )
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: '20px',
+  };
+
+  const headingStyle = {
+    color: '#3BBA9C',
+    fontSize: '3rem', 
+    marginTop: '-10%',
+    width: '100%', 
+    textAlign: 'center',
+  };
+
+  const listStyle = {
+    textAlign: 'left',
+    listStyle: 'disc',
+    width: '100%', 
+    fontSize: '1rem', 
+    marginLeft: '0',
+    paddingLeft: '2.5%', 
+  };
+
+  const listItemStyle = {
+    color: 'lightgrey',
+    fontSize: '1.25rem',
+    marginBottom: '3.5%',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <h1 style={headingStyle}>System Emulator</h1>
+      <ul style={listStyle}>
+        <li style={listItemStyle}>
+          Using C, I implemented a basic System Emulator that processed Assembly instructions and replicated a cache
+          system
+        </li>
+        <li style={listItemStyle}>
+          I implemented Instruction Pipelining, including fetching, decoding, and executing assembly instructions that
+          were given. In addition, I handled any data or control hazards that arose while processing
+        </li>
+        <li style={listItemStyle}>
+          In addition, I implemented a basic cache system, that used the LRU policy to evict and bring in new cache lines
+          into the cache
+        </li>
+      </ul>
+    </div>
+  );
 }
