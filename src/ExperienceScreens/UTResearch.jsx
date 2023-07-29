2b49c4c0aac51fe8fda12d328d9476b37fc10b44
AyanGupta516 <109633512+AyanGupta516@users.noreply.github.com>
2023-07-29 11:38:21 -0500

    Changes

--- src/ExperienceScreens/UTResearch.jsx
+++ src/ExperienceScreens/UTResearch.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 export default function UTResearch() {
     return (
-        <>
-            <h1 style = {{color: '#3BBA9C', position: 'absolute', top: '7.5%', left:'50%', transform: 'translateX(-50%)', width: '800px'}}> Galaxy Classification Research </h1>
-            <div style = {{width: '1150px', marginTop: '-25px'}}>
-                <h1 style = {{color: '#BB8FCE', fontSize: '32px', textAlign: 'left', marginTop: '60px', borderBottom: 'solid', width: '300px'}}> Summer 2023 Work </h1>
-                <ul style = {{textAlign: 'left', listStyle: 'disc', marginLeft: '-25px', width: '1250px', marginTop: '30px'}}>
-                    <li style = {{color: 'lightgrey', fontSize: '18px', marginBottom: '20px'}}> Furtherned my knowledge of astronomical concepts through SDSS explorations, readings, and online courses</li>
-                    <li style = {{color: 'lightgrey', fontSize: '18px', marginBottom: '20px'}}> Built an intermediate understanding of ML through Stanford's Machine Learning Specialization CourseEra course </li>
-                    <li style = {{color: 'lightgrey', fontSize: '18px', marginBottom: '20px'}}> Developed a research problem to investigate in the fall, familarizing myself with the topic and potential approaches through past academic papers </li>
+        <div style = {{display: 'flex', flexDirection: 'column', aligItems: 'center', 'alignItems': 'center'}}>
+            <h1 style = {{color: '#3BBA9C', width: '60vw', fontSize: '3.5vw', marginTop: '-2.5%'}}> Galaxy Classification Research </h1>
+            <div style = {{width: '90vw', marginTop: '-2.5%'}}>
+                <h1 style = {{color: '#BB8FCE', fontSize: '2rem', textAlign: 'left', marginTop: '4%', borderBottom: 'solid', width: '21vw'}}> Summer 2023 Work </h1>
+                <ul style = {{textAlign: 'left', listStyle: 'disc', marginLeft: '-2.5%', width: '90vw', marginTop: '2%'}}>
+                    <li style = {{color: 'lightgrey', fontSize: '1.15rem', marginBottom: '1.25rem'}}> Furtherned my knowledge of astronomical concepts through SDSS explorations, readings, and online courses</li>
+                    <li style = {{color: 'lightgrey', fontSize: '1.15rem', marginBottom: '1.25rem'}}> Built an intermediate understanding of ML through Stanford's Machine Learning Specialization CourseEra course </li>
+                    <li style = {{color: 'lightgrey', fontSize: '1.15rem', marginBottom: '1.25rem'}}> Developed a research problem to investigate in the fall, familarizing myself with the topic and potential approaches through past academic papers </li>
                 </ul>
-                <h1 style = {{color: '#BB8FCE', fontSize: '32px', textAlign: 'left', marginTop: '60px', borderBottom: 'solid', width: '225px'}}> Fall 2023 Work </h1>
-                <ul style = {{textAlign: 'left', listStyle: 'disc', marginLeft: '-25px', width: '1250px', marginTop: '30px'}}>
-                    <li style = {{color: 'lightgrey', fontSize: '18px', marginBottom: '20px'}}> Working collaboratively in a team of three, we are conducting research on the clustering of galaxies based on their emission lines </li>
-                    <li style = {{color: 'lightgrey', fontSize: '18px', marginBottom: '20px'}}>  Our study involves utilizing data from the Sloan Digital Sky Survey (SDSS) to develop a Machine Learning clustering algorithm</li>
-                    <li style = {{color: 'lightgrey', fontSize: '18px', marginBottom: '20px'}}> The primary goal of this project is to write a research paper for publication </li>
+                <h1 style = {{color: '#BB8FCE', fontSize: '2rem', textAlign: 'left', marginTop: '4%', borderBottom: 'solid', width: '16vw'}}> Fall 2023 Work </h1>
+                <ul style = {{textAlign: 'left', listStyle: 'disc', marginLeft: '-2.5%', width: '90vw', marginTop: '2%'}}>
+                    <li style = {{color: 'lightgrey', fontSize: '1.15rem', marginBottom: '1.25rem'}}> Working collaboratively in a team of three, we are conducting research on the clustering of galaxies based on their emission lines </li>
+                    <li style = {{color: 'lightgrey', fontSize: '1.15rem', marginBottom: '1.25rem'}}>  Our study involves utilizing data from the Sloan Digital Sky Survey (SDSS) to develop a Machine Learning clustering algorithm</li>
+                    <li style = {{color: 'lightgrey', fontSize: '1.15rem', marginBottom: '1.25rem'}}> The primary goal of this project is to write a research paper for publication </li>
                 </ul>
             </div>
-        </>
+        </div>
     )
 }
